d0feb80cc004c4a45219c20f848d9faca570df09
Brian West <bnwest@rice.edu>
2010-03-17 20:02:27 +0000

    making RhaptosSword a tool - part 2.

---
+++ Products/RhaptosSword/config.py
@@ -0,0 +1,13 @@
+"""
+Product configuration
+
+Author: Brian N West (bnwest@rice.edu)
+copyright (C) 2010, Rice University. All rights reserved.
+
+This software is subject to the provisions of the GNU Lesser General
+Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
+"""
+
+PROJECTNAME = "RhaptosSword"    # this and an import in Extensions.Install should be the only places product name appears
+
+GLOBALS = globals()
