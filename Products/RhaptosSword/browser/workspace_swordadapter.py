3b6c41e4d1d784d2987f9cd2d537b91d02e5e2ca
rijk <rijk@cnx.rice.edu>
2011-08-08 09:58:57 +0000

    Move back to using skin update_metadata, but do some metadata checking and cleaning beforehand.

--- Products/RhaptosSword/browser/workspace_swordadapter.py
+++ Products/RhaptosSword/browser/workspace_swordadapter.py
@@ -19,60 +19,55 @@ class IRhaptosWorkspaceSwordAdapter(ISWORDContentUploadAdapter):
     """
 
 
+def validateLicense(context, license_url):
+    """
+    We try to lookup the license details based on the url.
+    If we don't have this license in the database, it will raise an
+    exception. The whole process should stop at that point.
+    """
+    mdt = getToolByName(context, 'portal_moduledb')
+    mdt.getLicenseData(license_url)
+    return license_url
+
+
+
 class RhaptosWorkspaceSwordAdapter(PloneFolderSwordAdapter):
     """ Rhaptos specific implement of the SWORD folder adapter.
     """
     adapts(IFolderish, IHTTPRequest)
+    
+    METADATA_MAPPING =\
+            {'title'   : 'title',
+             'keywords': 'keywords',
+             'abstract': 'abstract',
+             'language': 'language',
+             'subject' : 'subject',
+             'license' : 'license',
+             'googleAnalyticsTrackingCode': 'GoogleAnalyticsTrackingCode',
+            }
+    
+    VALIDATORS =\
+            {'license': validateLicense,}
+
 
     def updateObject(self, obj, filename, request, response, content_type):
         if content_type in self.ATOMPUB_CONTENT_TYPES:
             body = request.get('BODYFILE')
             body.seek(0)
             dom = parse(body)
-            headers = self.getHeaders(dom, self.METADATA_MAPPING)
-            metadata = {}
-            for key, value in headers:
-                if value: metadata[key] = value
+
             obj = obj.__of__(self.context)
-            self.updateMetadata(obj, metadata)
+            metadata = self.getMetadata(dom, self.METADATA_MAPPING)
+            obj.update_metadata(**metadata)
             obj.reindexObject(idxs=metadata.keys())
         return obj
 
-    def updateMetadata(self, obj, metadata):
-        title = metadata.get('title', '').strip()
-        abstract = metadata.get('abstract', '').strip()
-        language = metadata.get('language', '').strip()
-        subject = metadata.get('subject', [])
-        license = metadata.get('license', '').strip()
-        keywords = [kw.strip() for kw in metadata.get('keywords', [])\
-                    if kw.strip()]
-        GoogleAnalyticsTrackingCode =\
-                metadata.get('GoogleAnalyticsTrackingCode', '').strip()
 
-        # Uniqify and remove empty items from list
-        keywords = filter(None, dict(map(None,keywords,[])).keys())
-        keywords.sort(lambda x,y: cmp(x.lower(),y.lower()))
-
-        obj.manage_changeProperties(language=language)
-        obj.manage_changeProperties({'title' : title,
-                                     'abstract' : abstract,
-                                     'keywords' : keywords,
-                                     'revised' : DateTime(),
-                                     'subject' : subject
-                                    })
-        
-        if license:
-            try:
-                mdt = getToolByName(self.context, 'portal_moduledb')
-                mdt.getLicenseData(license)
-                obj.setLicense(license)
-            except IndexError:
-                # TODO: Fixme: we should raise some error, etc. here
-                license = None
-            except AttributeError:
-                obj.manage_changeProperties(license=license)
-
-        if GoogleAnalyticsTrackingCode:
-            obj.setGoogleAnalyticsTrackingCode(GoogleAnalyticsTrackingCode)
-
-        obj.editMetadata()
+    def getMetadata(self, dom, mapping):
+        headers = self.getHeaders(dom, mapping)
+        metadata = {}
+        for key, value in headers:
+            if key == 'license':
+                value = validateLicense(self.context, value)
+            if value: metadata[key] = value
+        return metadata
