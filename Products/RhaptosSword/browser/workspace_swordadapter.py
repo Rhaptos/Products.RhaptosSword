05939b88034a4c2b1112620da291c850695ee880
rijk <rijk@cnx.rice.edu>
2011-08-10 06:34:19 +0000

    Add user details to receipt.

--- Products/RhaptosSword/browser/workspace_swordadapter.py
+++ Products/RhaptosSword/browser/workspace_swordadapter.py
@@ -158,10 +158,32 @@ class DepositReceiptAdapter(object):
 
 
 class DepositReceipt(BrowserView):
+    """ This extends the SWORD v 2.0 Deposit Receipt to:
+        - List role requests.
+        - Show whether the license has been signed by the author and all
+            contributors.
+        - What contributors (see above: author, editor, etc), still need
+            to accept their roles on the document.
+        - Show whether all required metadata has been provided (including
+            the ‘description-of-changes’ if this is a new version of
+            existing content).
+
+        TODO:
+        Decide how to handle differences between "In-Progress: true" and
+        "In-Progress: false" HTTP headers.
+    """
     implements(ISWORDDepositReceipt)
 
     def __call__(self):
         """ Temp call method as debug hook.
         """
         return self.index()
+    
 
+    def pending_collaborations(self):
+        return self.context.getPendingCollaborations()
+
+
+    def get_user(self, userid):
+        pmt = getToolByName(self.context, 'portal_membership')
+        return pmt.getMemberById(userid)
