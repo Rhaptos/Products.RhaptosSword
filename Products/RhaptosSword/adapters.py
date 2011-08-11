690ca5903a4ce7c08f59452c28d3fd430f043026
Izak Burger <izak@cnx.rice.edu>
2011-08-11 11:57:10 +0000

    If its a zip file, call the old import functionality. This is a direct copy of the old code.

--- Products/RhaptosSword/adapters.py
+++ Products/RhaptosSword/adapters.py
@@ -1,13 +1,16 @@
 from xml.dom.minidom import parse
+from zipfile import BadZipfile
 
 from zope.interface import Interface, implements
 from zope.publisher.interfaces.http import IHTTPRequest
 from zope.component import adapts, queryAdapter
+from AccessControl import getSecurityManager
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.interfaces import IFolderish
 
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CNXMLTransforms.helpers import OOoImportError, doTransform, makeContent
 
 from rhaptos.swordservice.plone.interfaces import ISWORDDepositReceipt
 from rhaptos.swordservice.plone.browser.sword import PloneFolderSwordAdapter
@@ -43,16 +46,49 @@ class RhaptosWorkspaceSwordAdapter(PloneFolderSwordAdapter):
     adapts(IFolderish, IHTTPRequest)
     
     def updateObject(self, obj, filename, request, response, content_type):
+        body = request.get('BODYFILE')
+        body.seek(0)
+        obj = obj.__of__(self.context)
         if content_type in self.ATOMPUB_CONTENT_TYPES:
-            body = request.get('BODYFILE')
-            body.seek(0)
             dom = parse(body)
-
-            obj = obj.__of__(self.context)
             metadata = self.getMetadata(dom, METADATA_MAPPING)
             obj.update_metadata(**metadata)
             #self.addRoles(obj, dom)
             obj.reindexObject(idxs=metadata.keys())
+        elif content_type == 'application/zip':
+            try:
+                kwargs = {
+                    'original_file_name': 'sword-import-file',
+                    'user_name': getSecurityManager().getUser().getUserName()
+                }
+                text, subobjs, meta = doTransform(obj, "sword_to_folder",
+                    body.read(), meta=1, **kwargs)
+                if text:
+                  obj.manage_delObjects([obj.default_file,])
+
+                  obj.invokeFactory('CNXML Document', obj.default_file, file=text, idprefix='zip-')
+                makeContent(obj, subobjs)
+
+                # Parse the returned mdml and set attributes up on the ModuleEditor object
+                # Add any additional, unmatched, aka uncredited authors
+                props = meta['properties']
+                obj.updateProperties(props)
+                # Make sure the metadata gets into the cnxml
+                obj.editMetadata()
+            except OOoImportError, e:
+                transaction.abort()
+                context.plone_log("SWORD Import for %s with id=%s: Aborted. There were problems transforming the openoffice or word document." % (memberId, new_id))
+                message = context.translate("message_could_not_import", {"errormsg":e}, domain="rhaptos",
+                                            default="Could not import file. %s" % e)
+                response.setStatus('BadRequest')
+                return state.set(status='SwordImportError', portal_status_message=message)
+            except BadZipfile, e:
+                transaction.abort()
+                context.plone_log("SWORD Import for %s with id=%s: Aborted. There were problems with the uploaded zip file." % (memberId, new_id))
+                response.setStatus('BadRequest')
+                state.setStatus('SwordErrorZip')
+                return state.set(context=context)
+                
         return obj
 
 
