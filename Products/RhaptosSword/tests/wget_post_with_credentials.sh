f0a5133f3cc737735bc8b9f6441b81979e0251b6
rijk <rijk@cnx.rice.edu>
2011-09-06 11:18:00 +0000

    Trying to make it more legible.

--- Products/RhaptosSword/tests/wget_post_with_credentials.sh
+++ Products/RhaptosSword/tests/wget_post_with_credentials.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-wget -q -O- --user=$1 --password=$1 --auth-no-challenge --header='Content-Type: multipart/related; boundary="===============1338623209=="' --header="Slug: testmodule" --header="In-Progress: True" --post-file=$3 $4
+wget -q -O- --user=$1 --password=$1 \
+--auth-no-challenge
+--header='Content-Type: multipart/related; boundary="===============1338623209=="'\
+--header="Slug: testmodule"\
+--header="In-Progress: True"\
+--post-file=$3 $4
