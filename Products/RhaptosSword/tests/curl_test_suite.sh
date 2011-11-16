21f32e2535c54d009c99e78b964cea9f190c2697
rijk <rijk@cnx.rice.edu>
2011-11-16 06:43:32 +0000

    Add some curl examples for new functionality.

--- Products/RhaptosSword/tests/curl_test_suite.sh
+++ Products/RhaptosSword/tests/curl_test_suite.sh
@@ -2,3 +2,9 @@
 
 # Create a module with a simple atom entry post
 ./curl_atom_post_create.sh roche brightred ./data/atomentry.xml http://localhost:8080/site
+
+# Add an existing module to an existing lens
+curl --request POST --header "Content-Type: application/atom+xml;type=entry" --header "In-Progress: true" --user [username]:[password] --upload-file [path to xml file] http://[Lens URL]/sword
+
+# example
+# curl --request POST --header "Content-Type: application/atom+xml;type=entry" --header "In-Progress: true" --user rijk:12345 --upload-file ./entry_add_to_lens.xml http://localhost:8080/site/lenses/rijk/rijk-stofbergs-lens/sword
